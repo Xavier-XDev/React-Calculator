9b12fb2ca51ccc02f2d58e741a4750f64f26f12e
Xavier-XDev <86858724+Xavier-XDev@users.noreply.github.com>
2023-04-25 02:07:27 +0200

    Delete calculator directory

--- calculator/src/components/ButtonBox.js
+++
@@ -1,8 +0,0 @@
-import "./ButtonBox.css";
-import React from "react";
-
-const ButtonBox = ({ children }) => {
-  return <div className="buttonBox">{children}</div>;
-};
-
-export default ButtonBox;
--- calculator/src/components/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import "./Wrapper.css";
-import React from "react";
-
-const Wrapper = ({ children }) => {
-  return <div className="wrapper">{children}</div>;
-};
-
-export default Wrapper;
